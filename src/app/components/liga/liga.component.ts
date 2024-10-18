e71f9171777053b694349ec5f93667a5b7b79601
Mauritz Langer <mauritz@langer-net.de>
2024-10-18 16:51:30 +0200

    Adding a Filter to the Games and Scorers

--- src/app/components/liga/liga.component.ts
+++ src/app/components/liga/liga.component.ts
@@ -7,7 +7,7 @@ import {
   MatHeaderCell, MatHeaderCellDef,
   MatHeaderRow, MatHeaderRowDef,
   MatRow, MatRowDef,
-  MatTable
+  MatTable, MatTableDataSource
 } from "@angular/material/table";
 import {Games} from "../../models/games";
 import {LigaService} from "../../services/liga/liga.service";
@@ -16,6 +16,8 @@ import {Table} from "../../models/table";
 import {NgIf} from "@angular/common";
 import {Scorer} from "../../models/scorer";
 import {animate, state, style, transition, trigger} from "@angular/animations";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-liga',
@@ -33,7 +35,10 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
     MatRowDef,
     MatTabGroup,
     MatTab,
-    NgIf
+    NgIf,
+    MatFormField,
+    MatInput,
+    MatLabel
   ],
   animations: [
     trigger('detailExpand', [
@@ -50,11 +55,11 @@ export class LigaComponent implements OnInit {
   ligaName: string = '';
 
   displayedColumnsGames: string[] = ['start', 'home', 'guest', 'location', 'result'];
-  dataSourceGames: Games[] = [];
+  dataSourceGames: MatTableDataSource<Games> = new MatTableDataSource();
   displayedColumnsTable: string[] = ['place', 'team', 'games', 'wins', 'draws', 'losses', 'goals', 'goalDifference', 'points'];
-  dataSourceTable: Table[] = [];
-  displayedColumnsScorer: string[] = ['place', 'player', 'team', 'goals', 'games'];
-  dataSourceScorer: Scorer[] = [];
+  dataSourceTable: MatTableDataSource<Table> = new MatTableDataSource();
+  displayedColumnsScorer = ['place', 'player', 'team', 'goals', 'games'];
+  dataSourceScorer: MatTableDataSource<Scorer> = new MatTableDataSource();
 
   expandedElement: Table | null = null;
   expandedGames: Games[] = [];
@@ -69,9 +74,9 @@ export class LigaComponent implements OnInit {
         (html: string) => {
           // Process the HTML content to extract items
           this.ligaName = this.ligaService.getLigaName(html);
-          this.dataSourceGames = this.ligaService.parseHtmlToGames(html)
-          this.dataSourceTable = this.ligaService.parseHtmlToTable(html)
-          this.dataSourceScorer = this.ligaService.parseHtmlToScorer(html)
+          this.dataSourceGames.data = this.ligaService.parseHtmlToGames(html)
+          this.dataSourceTable.data = this.ligaService.parseHtmlToTable(html)
+          this.dataSourceScorer.data = this.ligaService.parseHtmlToScorer(html)
         }
       );
     });
@@ -86,7 +91,7 @@ export class LigaComponent implements OnInit {
   }
 
   getGamesByRow(row: Table) {
-    this.expandedGames = this.dataSourceGames.filter(game => (game.home === row.team || game.guest === row.team) && game.result.includes(':'));
+    this.expandedGames = this.dataSourceGames.data.filter(game => (game.home === row.team || game.guest === row.team) && game.result.includes(':'));
   }
 
   checkEventInPast(start: string): boolean {
@@ -106,5 +111,15 @@ export class LigaComponent implements OnInit {
     } catch (error) {
       return false;
     }
+  }
+
+  applyGamesFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceGames.filter = filterValue.trim().toLowerCase();
+  }
+
+  applyScorerFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceScorer.filter = filterValue.trim().toLowerCase();
   }
 }
