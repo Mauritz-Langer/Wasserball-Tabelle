4b68bf6b9f5f29345f724e7399aaa46c1decabfc
Mauritz Langer <mauritz@langer-net.de>
2025-11-05 13:34:18 +0100

    Refactor AppComponent by removing unused navigation button and imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
-import {Router, RouterOutlet} from '@angular/router';
-import {MatIcon} from "@angular/material/icon";
-import {MatButton, MatIconButton} from "@angular/material/button";
+import {RouterOutlet} from '@angular/router';
 import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
@@ -9,20 +7,10 @@ import {MatToolbar} from "@angular/material/toolbar";
   standalone: true,
   imports: [
     MatToolbar,
-    RouterOutlet,
-    MatIconButton,
-    MatIcon,
-    MatButton
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
-  constructor(private router: Router) {
-  }
-
-  navigateToStart(): void {
-    this.router.navigate(['/']);
-  }
 }
